ea8d7c133e36e25f33f6e74929f7ee2bcac1a115
Willy Aguirre <marti1125@gmail.com>
2020-12-06 23:08:46 -0300

    added some examples

---
+++ c_programming_fundamentals/poem.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{   
+    printf("\"No Man Is An Island\" by John Donne\n\n"
+        "No man is an island,\n"
+        "Entire of itself,\n"
+        "Every man is a piece of the continent,\n"
+        "A part of the main.\n"
+        "If a clod be washed away by the sea,\n"
+        "Europe is the less.\n"
+        "As well as if a promontory were.\n"
+        "As well as if a manor of thy friend’s\n"
+        "Or of thine own were:\n"
+        "Any man’s death diminishes me,\n"
+        "Because I am involved in mankind,\n"
+        "And therefore never send to know for whom the bell tolls;\n"
+        "It tolls for thee.\n");
+    return 0;
+} 
